58d39dcd7e55bbdd4500a31626d6a2f38c9c4185
KatherineB <kbiefer@golden.net>
2020-08-29 16:55:44 -0400

    changed names of some variables that were confusingly named and fixed errors in math

--- C++/BedReserve/BedReserve.cpp
+++ C++/BedReserve/BedReserve.cpp
@@ -17,12 +17,14 @@ int main ()
 	int blank;
 
 	int TOTAL_REG_BEDS = 2700;
-	int TOTAL_ICU_BEDS = 430;   // separate from reg beds
-	int TOTAL_ICU_VENT_BEDS = 115;  // included within the ICU beds, ie 115 of the 430 icu beds have ventilators
+	int TOTAL_ICU_BEDS = 120;   // separate from reg beds
+	int TOTAL_ICU_VENT_BEDS = 75;  // included within the ICU beds, ie 75 of the 120 icu beds have ventilators
 	
+	int general_trial_beds_needed;
+
 	double PERCENT_REG_NEEDED = 0.80;
-	double PERCENT_ICU_NEEDED = 0.70;
-	double PERCENT_ICU_VENT_NEEDED = 0.07;
+	double PERCENT_ICU_NEEDED = 0.55;
+	double PERCENT_ICU_VENT_NEEDED = 0.15;
 
 	int normal_reg_beds_needed = int(TOTAL_REG_BEDS * PERCENT_REG_NEEDED);
 	int normal_icu_beds_needed = int(TOTAL_ICU_BEDS * PERCENT_ICU_NEEDED); 
@@ -37,7 +39,7 @@ int main ()
 	int emerg_trial_icu_vent_beds_needed; // percentage
 
 	int emerg_icu_beds_needed;
-	int emerg_icu_vent_reg_beds_needed;
+	int emerg_icu_vent_beds_needed;
 
 	int regBedsLeft;
 	int icuBedsLeft;
@@ -52,18 +54,19 @@ int main ()
 	cout << "Number of ICU Beds that have ventilators: " << TOTAL_ICU_VENT_BEDS << endl;
 	cout << "Average Number of Regular Beds Occupied: " << normal_reg_beds_needed << endl;
 	cout << "Average Number of ICU Beds Occupied: " << normal_icu_beds_needed << endl;
+	cout << "Average Number of ICU Beds With Ventilators Occupied: " << normal_icu_vent_beds_needed << endl;
     cout << endl;
 
 	cout << "Enter trial number that may be hospitalized due to emergency: " ;
-	cin >> emerg_trial_reg_beds_needed;
+	cin >> general_trial_beds_needed;
 	cout << endl;
 
-	
-	regBedsLeft = TOTAL_REG_BEDS - normal_reg_beds_needed - emerg_trial_reg_beds_needed;
 	/*
-	cout << "Number of regular beds occupied due to emergency: " << emerg_trial_reg_beds_needed << endl;
+	regBedsLeft = TOTAL_REG_BEDS - normal_reg_beds_needed - emerg_trial_reg_beds_needed;	
+	cout << "Number of regular beds occupied due to emergency: " << emerg_trial_reg_beds_needed << endl;	
 	cout << "Regular hospital beds remaining in hospital: " << regBedsLeft << endl << endl;
-    */
+    cout << endl;
+	*/
 
 	cout << "Enter percentage that may require ICU beds: " ;
 	cin >> emerg_trial_icu_beds_needed;
@@ -71,29 +74,31 @@ int main ()
 
 	//cout << TOTAL_ICU_BEDS << endl;
 	//cout << normal_icu_beds_needed << endl;
-	cout << emerg_trial_reg_beds_needed << endl;
+	cout << general_trial_beds_needed << endl;
 	cout <<	emerg_trial_icu_beds_needed << endl;
-	emerg_icu_beds_needed = emerg_trial_reg_beds_needed * emerg_trial_icu_beds_needed / 100;
+	emerg_icu_beds_needed = general_trial_beds_needed * emerg_trial_icu_beds_needed / 100;
 	cout << emerg_icu_beds_needed << endl;
 	
 	icuBedsLeft = TOTAL_ICU_BEDS - normal_icu_beds_needed - emerg_icu_beds_needed;
-	emerg_trial_reg_beds_needed = emerg_trial_reg_beds_needed - emerg_icu_beds_needed;
-	regBedsLeft = regBedsLeft + emerg_trial_icu_beds_needed;
+	emerg_trial_reg_beds_needed = general_trial_beds_needed - emerg_icu_beds_needed;
+	regBedsLeft = TOTAL_REG_BEDS - normal_reg_beds_needed - emerg_trial_reg_beds_needed;
 
 	cout << "Number of regular beds occupied due to emergency: " << emerg_trial_reg_beds_needed << endl;
 	cout << "Number of ICU beds occupied due to emergency: " << emerg_icu_beds_needed << endl;
 	cout << "Regular hospital beds remaining in hospital: " << regBedsLeft << endl;
 	cout << "Number of ICU beds remaining in hospital: " << icuBedsLeft << endl;
 
-	 cout << "Enter percentage that may require ICU beds that have ventilators: " ;
+	 cout << "Enter percentage in ICU that may require ventilators: " ;
 	cin >> emerg_trial_icu_vent_beds_needed;
 	cout << endl;
+
+	emerg_icu_vent_beds_needed = emerg_icu_beds_needed * emerg_trial_icu_vent_beds_needed / 100;
 	
-	icuVentBedsLeft = TOTAL_ICU_VENT_BEDS - PERCENT_ICU_VENT_NEEDED - emerg_trial_icu_vent_beds_needed;
+	icuVentBedsLeft = TOTAL_ICU_VENT_BEDS - normal_icu_vent_beds_needed - emerg_icu_vent_beds_needed;
 	
 	cout << "Number of regular beds occupied due to emergency: " << emerg_trial_reg_beds_needed << endl;
 	cout << "Number of ICU beds occupied due to emergency: " << emerg_icu_beds_needed << endl;
-	cout << "number of ICU beds with ventilators occupied due to emergency: " << emerg_trial_icu_vent_beds_needed << endl;
+	cout << "Number of ICU beds with ventilators occupied due to emergency: " << emerg_icu_vent_beds_needed << endl;
 	cout << "Regular hospital beds remaining in hospital: " << regBedsLeft << endl;
 	cout << "Number of ICU beds remaining in hospital: " << icuBedsLeft << endl;
 	cout << "Number of ICU beds with ventilators remaining in hospital: " << icuVentBedsLeft << endl;
