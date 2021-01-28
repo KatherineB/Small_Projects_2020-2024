3f5500c18bb23cb8cdaf4f5307bada67099db450
KatherineB <kbiefer@golden.net>
2021-01-28 13:34:36 -0500

    added range selection for chosen random numbers

--- JavaScript/RandNumPic2021/RandNumPic2021.js
+++ JavaScript/RandNumPic2021/RandNumPic2021.js
@@ -7,13 +7,32 @@ var highestNumber = 100;
 var duplicatesAllowed = true;
 
 function pickRandNums(){
-var non = document.getElementById("numberOfNumbers");
-var choice = non.options[non.selectedIndex].text;	
+var non = document.getElementById("numberOfNumbers").value;
+numberOfNumbers = parseInt(non);
 
-numberOfNumbers = parseInt(choice);
+var s100 = document.getElementById("start-100").value;
+var s10 = document.getElementById("start-10").value;
+var s1 = document.getElementById("start-1").value;
+
+var e100 = document.getElementById("end-100").value;
+var e10 = document.getElementById("end-10").value;
+var e1 = document.getElementById("end-1").value;
+
+var start100 = parseInt(s100) * 100;
+var start10 = parseInt(s10) * 10;
+var start1 = parseInt(s1);
+
+var end100 = parseInt(e100) * 100;
+var end10 = parseInt(e10) * 10;
+var end1 = parseInt(e1);
+
+var start = start100 + start10 + start1;
+var end = end100 + end10 + end1;
+
+var difference = end - start;
 
 for (var i=0; i < numberOfNumbers; i++){
-    chosenNumbers[i] = Math.floor(Math.random() * highestNumber) + 1;
+    chosenNumbers[i] = Math.floor(Math.random() * (difference + 1)) + start;
    
     var div = document.createElement('div');    
     div.setAttribute('class', 'number-box');
